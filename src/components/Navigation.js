87ffc67212a891cdc5772cfd01d312ed9946eb53
masterasia1 <a.mayfield18@gmail.com>
2022-05-11 03:51:05 -0700

    created navigation bar

---
+++ src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Nav, Navbar} from 'react-bootstrap'
+import logo from '../favicon.jpeg'
+
+class Navigation extends Component {
+    render (){
+        return (
+            <div>
+                <Navbar bg='dark' variant="dark" sticky="top" expand='md' collapseOnSelect>
+                <Navbar.Brand
+
+
+
+                </Navbar>
+            
+            </div>
+        )
+    }
+}
